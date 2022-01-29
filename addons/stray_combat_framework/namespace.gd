b343f21d89a06e99606e517d32f143fa758ade46
Ezekiel Halley <eze.halley@gmail.com>
2022-01-29 11:32:31 -0500

    renamed hit_detection folder to collision_detection

--- addons/stray_combat_framework/namespace.gd
+++ addons/stray_combat_framework/namespace.gd
@@ -1,18 +1,18 @@
 class_name StrayCF
 
 # Combat - Hit Detection
-const CharacterBody2D = preload("src/combat/hit_detection/2d/body/character_body_2d.gd")
-const FighterBody2D = preload("src/combat/hit_detection/2d/body/fighter_body_2d.gd")
-const PushBox2D = preload("src/combat/hit_detection/2d/body/push_box_2d.gd")
-const HitBox2D = preload("src/combat/hit_detection/2d/hit_box_2d.gd")
-const BoxSwitcher2D = preload("src/combat/hit_detection/2d/box_switcher_2d.gd")
-const HitState2D = preload("src/combat/hit_detection/2d/hit_state_2d.gd")
-const HitStateController2D = preload("src/combat/hit_detection/2d/hit_state_controller_2d.gd")
+const CharacterBody2D = preload("src/combat/collision_detection/2d/body/character_body_2d.gd")
+const FighterBody2D = preload("src/combat/collision_detection/2d/body/fighter_body_2d.gd")
+const PushBox2D = preload("src/combat/collision_detection/2d/body/push_box_2d.gd")
+const HitBox2D = preload("src/combat/collision_detection/2d/hit_box_2d.gd")
+const BoxSwitcher2D = preload("src/combat/collision_detection/2d/box_switcher_2d.gd")
+const HitState2D = preload("src/combat/collision_detection/2d/hit_state_2d.gd")
+const HitStateController2D = preload("src/combat/collision_detection/2d/hit_state_controller_2d.gd")
 
 # Combat - Hit Attributes
-const HitAttributes = preload("src/combat/hit_detection/hit_attributes/hit_attributes.gd")
-const AttackAttributes = preload("src/combat/hit_detection/hit_attributes/attack_attributes.gd")
-const HurtAttributes = preload("src/combat/hit_detection/hit_attributes/hurt_attributes.gd")
+const HitAttributes = preload("src/combat/collision_detection/hit_attributes/hit_attributes.gd")
+const AttackAttributes = preload("src/combat/collision_detection/hit_attributes/attack_attributes.gd")
+const HurtAttributes = preload("src/combat/collision_detection/hit_attributes/hurt_attributes.gd")
 
 # Combat - State Management
 const FighterState = preload("src/combat/fsm_states/fighter_state.gd")
