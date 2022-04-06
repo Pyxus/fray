8371aeac5f898729155fba5a568f745f81bbfb89
Ezekiel Halley <eze.halley@gmail.com>
2022-04-06 02:13:38 -0400

    Reorganized lib folder

--- addons/stray_combat_framework/lib/helpers/child_change_detector.gd
+++ addons/stray_combat_framework/lib/helpers/child_change_detector.gd
@@ -9,7 +9,7 @@ enum Change{
 	SCRIPT_CHANGED,
 }
 
-const Util = preload("res://addons/stray_combat_framework/lib/utils/util.gd")
+const SignalUtils = preload("utils/signal_utils.gd")
 
 var _parent: Node
 
@@ -20,22 +20,22 @@ func _init(parent: Node) -> void:
 
 	var scene_tree = parent.get_tree()
 	_parent = parent
-	Util.safe_connect(scene_tree, "node_added", self, "_on_SceneTree_node_added")
-	Util.safe_connect(scene_tree, "node_removed", self, "_on_SceneTree_node_removed")
+	SignalUtils.safe_connect(scene_tree, "node_added", self, "_on_SceneTree_node_added")
+	SignalUtils.safe_connect(scene_tree, "node_removed", self, "_on_SceneTree_node_removed")
 
 
 func _on_SceneTree_node_added(node: Node) -> void:
 	if node.get_parent() == _parent:
 		emit_signal("child_changed", node, Change.ADDED)
-		Util.safe_connect(node, "script_changed", self, "_on_ChildNode_script_changed", [node])
-		Util.safe_connect(node, "renamed", self, "_on_ChildNode_renamed", [node])
+		SignalUtils.safe_connect(node, "script_changed", self, "_on_ChildNode_script_changed", [node])
+		SignalUtils.safe_connect(node, "renamed", self, "_on_ChildNode_renamed", [node])
 
 		
 func _on_SceneTree_node_removed(node: Node) -> void:
 	if node.get_parent() == _parent:
 		emit_signal("child_changed", node, Change.REMOVED)
-		Util.safe_disconnect(node, "script_changed", self, "_on_ChildNode_script_changed")
-		Util.safe_disconnect(node, "renamed", self, "_on_ChildNode_renamed")
+		SignalUtils.safe_disconnect(node, "script_changed", self, "_on_ChildNode_script_changed")
+		SignalUtils.safe_disconnect(node, "renamed", self, "_on_ChildNode_renamed")
 
 
 func _on_ChildNode_script_changed(node: Node) -> void:
