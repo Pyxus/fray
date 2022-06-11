4011b8fa6710db333e934243f14d17cd9ddcdb42
Ezekiel Halley <eze.halley@gmail.com>
2022-06-11 02:40:06 -0400

    Revert "Removed unused property in InputButtonCondition"
    This reverts commit fcf96973d8625fb87cb7838b2da096e0746266d7.

--- addons/fray/src/combat_state/transitions/conditions/input_button_condition.gd
+++ addons/fray/src/combat_state/transitions/conditions/input_button_condition.gd
@@ -1,18 +1,23 @@
 extends "input_condition.gd"
 ## Class representing input button condition
 
+# Imports
 const BufferedInputButton = preload("../../buffered_input/buffered_input_button.gd")
 
 ## Input id
 export var input_id: int
 
+## If true the condition only counts the input if it is released
+export var is_triggered_on_release: bool #TODO: Reimplement, currently this condition is ignored by the only analyzer implementation
 
-func _init(input_id: int = -1) -> void:
+
+func _init(input_id: int = -1, is_triggered_on_release: bool = false) -> void:
 	self.input_id = input_id
+	self.is_triggered_on_release = is_triggered_on_release
 
 
 func is_satisfied_by(buffered_input: BufferedInput) -> bool:
 	if buffered_input is BufferedInputButton:
-		return buffered_input.id == input_id
+		return buffered_input.id == input_id and buffered_input.is_pressed != is_triggered_on_release
 
 	return false
