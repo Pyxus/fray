44a11b2f71f3403efcda7e47518cbc736d566a7c
Ezekiel <ehalle4@students.towson.edu>
2023-03-17 21:49:41 -0400

    Combined global state machine  and standard state machine

--- addons/fray/src/state/node/state_node_state_machine.gd
+++ addons/fray/src/state/node/state_node_state_machine.gd
@@ -4,6 +4,15 @@ extends FrayStateNode
 ##
 ## Contains multiple multiple [FrayStateNode]s connected through [FrayStateMachineTransition]s.
 ## It is recommended to construct this state machine with a [FrayStateMachineBuilder].
+## [br][br]
+## Global transitions are a convinience feature that allows you to automatically connect states based on global transition rules.
+## Nodes within this state machine can be assigned tags, transition rules can then be set from one tag to another tag.
+## Nodes with a given 'from_tag' will automatically have a transition setup to global states with a given 'to_tag'.
+## A state is considered global if it is used as the 'to' state in a global transition.
+## [br][br]
+## This is useful for setting up transitions which need to be available from multiple states without needing to manually connect them.
+## For example, in many fighting games you could say all attacks tagged as 'normal' may transition into attacks tagged as 'special'
+
 
 ## Emitted when the current state is changes
 signal transitioned(from: StringName, to: StringName)
@@ -28,13 +37,21 @@ var current_node: StringName = "":
 		if _ERR_INVALID_NODE(node): return
 		goto(node)
 
-var _astar := _AStarGraph.new(_get_transition_priority)
-var _travel_args: Dictionary
-
 # Type: Dictionary<StringName, StateNode>
 var _states: Dictionary
 
+# Type: Dictionary<StringName, StringName[]>
+# Hint: <from tag, to tags>
+var _global_transition_rules: Dictionary
+
+# Type: Dictionary<StringName, StringName[]>
+# Hint: <node name, tags>
+var _tags_by_node: Dictionary
+
+var _astar := _AStarGraph.new(_get_transition_priority)
+var _travel_args: Dictionary
 var _transitions: Array[Transition]
+var _global_transitions: Array[FrayStateMachineTransition]
 
 
 func _enter_impl(args: Dictionary) -> void:
@@ -62,6 +79,10 @@ func remove_node(name: StringName) -> void:
 	var node: RefCounted = get_node(name)
 	_states.erase(name)
 	_astar.remove_point(name)
+	
+	if _tags_by_node.has(name):
+		_tags_by_node.erase(name)
+	
 	_on_node_removed(name, node)
 
 ## Renames the specified node.
@@ -75,6 +96,12 @@ func rename_node(old_name: StringName, new_name: StringName) -> void:
 	_states[new_name] = _states[old_name]
 	_states.erase(old_name)
 	_astar.rename_point(old_name, new_name)
+	
+	if _tags_by_node.has(old_name):
+		var tags: PackedStringArray = _tags_by_node[old_name]
+		_tags_by_node.erase(old_name)
+		_tags_by_node[new_name] = tags
+	
 	_on_node_renamed(old_name, new_name)
 
 ## Replaces the specified node's object.
@@ -231,6 +258,96 @@ func get_next_transitions(from: StringName) -> Array[Transition]:
 		if transition.from == from:
 			transitions.append(transition)
 
+	return transitions + get_next_global_transitions(from)
+
+## Sets the [kbd]tags[/kbd] associated with a [kbd]node[/kbd] if the node exists.
+func set_node_tags(node: StringName, tags: PackedStringArray) -> void:
+	if _ERR_INVALID_NODE(node): return
+	
+	_tags_by_node[node] = tags
+
+## Gets the tags associated with a [kbd]node[/kbd] if the node exists.
+func get_node_tags(node: StringName) -> PackedStringArray:
+	if _ERR_INVALID_NODE(node) or not _tags_by_node.has(node):
+		return PackedStringArray([])
+	
+	return _tags_by_node[node]
+
+## Returns [code]true[/code] if the given node is considered global.
+## [br]
+## A node is considered global if a global transition to the node exists.
+func is_node_global(node: StringName) -> bool:
+	if _ERR_INVALID_NODE(node): return false
+
+	for transition in _global_transitions:
+		if transition.to == node:
+			return true
+
+	return false
+
+## Adds global input transition to a state.
+func add_global_transition(to: StringName, transition: FrayStateMachineTransition) -> void:
+	if _ERR_INVALID_NODE(to): return
+	
+	var tr := Transition.new()
+	tr.to = to
+	tr.transition = transition
+
+	_global_transitions.append(tr)
+
+## Adds global transition rule based on tags.
+func add_global_transition_rule(from_tag: StringName, to_tag: StringName) -> void:
+	if not _global_transition_rules.has(from_tag):
+		_global_transition_rules[from_tag] = []
+
+	_global_transition_rules[from_tag].append(to_tag)
+
+## Removes a state's global transition.
+func remove_global_transition(to_state: StringName) -> void:
+	if not has_global_transition(to_state):
+		push_warning("Failed to remove global transition. State '%s' does not have a global transition")
+		return
+
+	for transition in _global_transitions:
+		if transition.to == to_state:
+			_global_transitions.erase(transition)
+			return
+
+## Returns [code]true[/code] if a state has a global transition.
+func has_global_transition(to_state: StringName) -> bool:
+	for transition in _global_transitions:
+		if transition.to == to_state:
+			return true
+	return false
+
+## Returns [code]true[/code] if global transition rule exists.
+func has_global_transition_rule(from_tag: StringName, to_tag: StringName) -> bool:
+	return _global_transition_rules.has(from_tag) and _global_transition_rules[from_tag].has(to_tag)
+
+## Removes specifc global transition rule from one tag to another.
+func remove_global_transition_rule(from_tag: StringName, to_tag: StringName) -> void:
+	if has_global_transition_rule(from_tag, to_tag):
+		_global_transition_rules.erase(to_tag)
+
+## Removes all global transitions from given tag.
+func delete_global_transition_rule(from_tag: StringName) -> void:
+	if _global_transition_rules.has(from_tag):
+		_global_transition_rules.erase(from_tag)
+
+## Returns array of next global transitions accessible from this state.
+func get_next_global_transitions(from: StringName) -> Array[FrayStateMachineTransition]:
+	if _ERR_INVALID_NODE(from): return []
+	
+	var transitions: Array[FrayStateMachineTransition]
+	
+	for from_tag in get_node_tags(from):
+		if _global_transition_rules.has(from_tag):
+			var to_tags: Array[StringName] = _global_transition_rules[from_tag]
+
+			for transition in _global_transitions:
+				for to_tag in to_tags:
+					if to_tag in get_node_tags(transition.to): 
+						transitions.append(transition)
 	return transitions
 
 ## Process child states then this state.
