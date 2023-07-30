a63bfac1722bc4d354308f45fc76d4fe4f39c31a
Ezekiel <ehalle4@students.towson.edu>
2023-07-29 23:18:12 -0400

    Update general_state_machine.gd

--- src/state_mgmt/general_state_machine.gd
+++ src/state_mgmt/general_state_machine.gd
@@ -2,7 +2,7 @@ class_name FrayGeneralStateMachine
 extends FrayStateMachine
 ## Basic implementation of [FrayStateMachine] intended for general purpose state management.
 ##
-## Contains a root propety, which can be updated to set  the root state. Once the root state is set, the state machine
+## Contains a root propety, which can be updated to set the root state. Once the root state is set, the state machine
 ## can begin processing state transitions.
 
 ## The root of this state machine.
