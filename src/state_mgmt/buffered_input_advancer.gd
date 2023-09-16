cbb523c0805710243d46f90c3eb5b7873635da17
Ezekiel <ehalle4@students.towson.edu>
2023-09-16 02:26:53 -0400

    Fix max_buffer_time being ignored

--- src/state_mgmt/buffered_input_advancer.gd
+++ src/state_mgmt/buffered_input_advancer.gd
@@ -1,11 +1,12 @@
 @icon("res://addons/fray/assets/icons/buffered_input_advancer.svg")
 class_name FrayBufferedInputAdvancer
 extends Node
-## A node which attempts to advance a given state machine using buffered inputs.
+## A node which attempts to advance a designated state machine using buffered inputs.
 
-enum AdvanceMode{
-	IDLE, ## Advance during the idle process
-	PHYSICS, ## Advance during the physics process
+
+enum AdvanceMode {
+	IDLE,  ## Advance during the idle process
+	PHYSICS,  ## Advance during the physics process
 }
 
 ## The state machine to be controlled by the advancer
@@ -35,6 +36,7 @@ func _physics_process(delta: float) -> void:
 	if advance_mode == AdvanceMode.PHYSICS:
 		_advance()
 
+
 ## Buffers an input button to be processed by the state machine
 ##
 ## [kbd]input[/kbd] is the name of the input.
@@ -45,6 +47,7 @@ func _physics_process(delta: float) -> void:
 func buffer_button(input: StringName, is_pressed: bool = true) -> void:
 	_input_buffer.append(BufferedInputButton.new(Time.get_ticks_msec(), input, is_pressed))
 
+
 ## Buffers an input sequence to be processed by the state machine
 #
 ## [kbd]sequence_name[/kbd] is the name of the sequence.
@@ -53,9 +56,11 @@ func buffer_button(input: StringName, is_pressed: bool = true) -> void:
 func buffer_sequence(sequence_name: StringName) -> void:
 	_input_buffer.append(BufferedInputSequence.new(Time.get_ticks_msec(), sequence_name))
 
+
 ## Clears the input buffer
 func clear_buffer() -> void:
 	_input_buffer.clear()
+
 
 ## Returns the current state of the buffer.
 ## The returned buffer objects are not copies.
@@ -63,7 +68,7 @@ func get_buffer() -> Array[BufferedInput]:
 	return _input_buffer.duplicate()
 
 
-func _advance()  -> void:
+func _advance() -> void:
 	var current_time := Time.get_ticks_msec()
 
 	while not _input_buffer.is_empty() and not paused:
@@ -71,12 +76,15 @@ func _advance()  -> void:
 		var time_since_last_input = current_time - _time_since_last_input_msec
 		var time_since_inputted: int = current_time - buffered_input.time_stamp
 		var advance_input := _create_advance_input(buffered_input, time_since_last_input)
-	
-		if state_machine.advance(advance_input):
+
+		if time_since_inputted <= max_buffer_time and state_machine.advance(advance_input):
 			_time_since_last_input_msec = current_time
 			break
 
-func _create_advance_input(buffered_input: BufferedInput, time_since_last_input: float) -> Dictionary:
+
+func _create_advance_input(
+	buffered_input: BufferedInput, time_since_last_input: float
+) -> Dictionary:
 	if buffered_input is BufferedInputButton:
 		return {
 			input = buffered_input.input,
@@ -91,23 +99,26 @@ func _create_advance_input(buffered_input: BufferedInput, time_since_last_input:
 		}
 	return {}
 
+
 class BufferedInput:
 	extends RefCounted
 
 	func _init(input_time_stamp: int = 0) -> void:
 		time_stamp = input_time_stamp
-	
+
 	var time_stamp: int
-	
+
 
 class BufferedInputButton:
 	extends BufferedInput
 
-	func _init(input_time_stamp: int = 0, input_name: StringName = "", input_is_pressed: bool = true) -> void:
+	func _init(
+		input_time_stamp: int = 0, input_name: StringName = "", input_is_pressed: bool = true
+	) -> void:
 		super(input_time_stamp)
 		input = input_name
 		is_pressed = input_is_pressed
-	
+
 	var input: StringName
 	var is_pressed: bool
 
@@ -118,5 +129,5 @@ class BufferedInputSequence:
 	func _init(input_time_stamp: int = 0, input_sequence_name: StringName = "") -> void:
 		super(input_time_stamp)
 		sequence_name = input_sequence_name
-	
+
 	var sequence_name: StringName
