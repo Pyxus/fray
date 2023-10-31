d0bc41cacb25c73b4412d48cb51a2e26e4781c96
pyxus <eze.halley@gmail.com>
2023-10-31 02:50:56 -0400

    Add msec_to_sec and sec_to_msec functions

--- src/fray.gd
+++ src/fray.gd
@@ -17,3 +17,11 @@ static func frames_to_msec(frames: int, fps: int = -1) -> int:
 static func msec_to_frames(msec: int, fps: int = -1) -> int:
 	fps = Engine.physics_ticks_per_second if fps <= 0 else fps
 	return ceil((Engine.physics_ticks_per_second * msec) / 1000.0)
+
+## Converts time in [kdb]milliseconds[/kbd] to time in seconds.
+static func msec_to_sec(msec: int) -> float:
+	return msec / 1000.0
+
+## Converts time in [kdb]seconds[/kbd] to time in milliseconds.
+static func sec_to_msec(sec: float) -> int:
+	return roundi(sec * 1000)
