be73a3d4e3b2d54564a6896aa6975436b207206d
pyxus <eze.halley@gmail.com>
2023-11-05 23:41:36 -0500

    Add usignal alternative to observer 'on' methods.
    'on' methods may be removed

--- src/state_mgmt/component/animation_observer.gd
+++ src/state_mgmt/component/animation_observer.gd
@@ -18,22 +18,43 @@ extends FrayStateMachineComponent
 func _ready() -> void:
 	if Engine.is_editor_hint():
 		return
-		
+
 	tracker.ready()
 
 
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		return
-		
+
 	tracker.process(delta)
 
 
 func _physics_process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		return
-		
+
 	tracker.physics_process(delta)
+
+
+## Returns a user defined signal which is used to connect to the start event of a given animation.
+## [br]
+## Signal excepts a method of type [code]func() -> void[/code]
+func usignal_started(animation: String) -> Signal:
+	return Signal(tracker, tracker.format_usignal_started(animation))
+
+
+## Returns a user defined signal which is used to connect to the finish event of a given animation.
+## [br]
+## Signal excepts a method of type [code]func() -> void[/code]
+func usignal_finished(animation: String) -> Signal:
+	return Signal(tracker, tracker.format_usignal_finished(animation))
+
+
+## Returns a user defined signal which is used to connect to the update event of a given animation.
+## [br]
+## Signal excepts a method of type [code]func(float) -> void[/code]
+func usignal_updated(animation: String) -> Signal:
+	return Signal(tracker, tracker.format_usignal_updated(animation))
 
 
 ## Used to connect to the start event of a given animation.
@@ -63,6 +84,7 @@ func _get_path_from(from_node: Node) -> NodePath:
 	return NodePath(from_node.get_path_to(self))
 
 
+# region Help wanted
 # These emit methods exist solely because I can't figure out how to call the AnimatorTracker methods on an animation track.
 # Which is necessary for my approach to animation tree tracking.
 func _emit_anim_started(animation: StringName) -> void:
@@ -72,5 +94,7 @@ func _emit_anim_started(animation: StringName) -> void:
 func _emit_anim_finished(animation: StringName) -> void:
 	tracker.emit_anim_finished(animation)
 
+
 func _emit_anim_updated(animation: StringName, play_position: float) -> void:
 	tracker.emit_anim_updated(animation, play_position)
+# endregion
