b778d598d2b6c382022c422c6fccca8bcadc51c6
pyxus <eze.halley@gmail.com>
2024-01-16 07:27:50 -0500

    Updaate observer doc comment

--- src/state_mgmt/component/animation_observer.gd
+++ src/state_mgmt/component/animation_observer.gd
@@ -4,7 +4,7 @@ extends Node
 ## A node used to observe animation events
 ##
 ## The animation observer can observe animation events from animators provided by the `FrayAnimatorTracker` resource.
-## These events can be subscribed to per-animation using the included 'on' methods.
+## These events can be subscribed to per-animation using the included 'usignal' methods.
 ## The tracker also has event signals which can be connected to in a way that isn't per-animation.
 
 ## Used to determine which animator to observe
@@ -41,7 +41,6 @@ func _get_configuration_warnings() -> PackedStringArray:
 
 	if tracker:
 		warnings += tracker._get_configuration_warnings_impl()
-		#print(tracker._get_configuration_warnings_impl())
 
 	return warnings
 
